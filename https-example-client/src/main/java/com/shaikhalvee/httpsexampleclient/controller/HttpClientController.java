bd9dd04c0bb69502a049e4324ffc536fbc54b3e5
Shaikh Islam <shaikhalvee@gmail.com>
2019-01-07 13:30:28 +0600

    if application.properties file containes ssl values{key, pass, keystore-path} client api must be called with https, if there is no ssl values client api can be called with http. But when client is in http mode, the https server certificate file must be imported into C:\Program Files\Java\jdk-10.0.1/lib/security/cacerts file, otherwise client will not be able to connect with https server.

--- https-example-client/src/main/java/com/shaikhalvee/httpsexampleclient/controller/HttpClientController.java
+++ https-example-client/src/main/java/com/shaikhalvee/httpsexampleclient/controller/HttpClientController.java
@@ -5,6 +5,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+// if application.properties file is added with the client-service ssl values
+// then it must be called with https://localhost:8091/data/{name}
+// and if application.properties file doesn't contain the ssl values {key, password, store-path}
+// it can be called with http://localhost:8091/data/{name}
 @RestController
 public class HttpClientController {
 
