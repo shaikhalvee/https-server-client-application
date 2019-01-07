6600e38b66233e656e1f8c005b3084aee181afab
Shaikh Islam <shaikhalvee@gmail.com>
2019-01-07 12:32:08 +0600

    Client can call https and http from an https & http server respectively

--- https-example-client/src/main/java/com/shaikhalvee/httpsexampleclient/controller/HttpClientController.java
+++ https-example-client/src/main/java/com/shaikhalvee/httpsexampleclient/controller/HttpClientController.java
@@ -1,8 +1,21 @@
 package com.shaikhalvee.httpsexampleclient.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class HttpClientController {
 
+	private static final String HTTP_SERVICE_URL = "http://localhost";
+	private static final String PORT = "8099";
+	private static final String API = "/goodbye/";
+	private static final String VALUE = "ShaikhIslamAlvee";
+
+	// Normal HTTP call
+	@GetMapping("/data")
+	public String getData() {
+		RestTemplate template = new RestTemplate();
+		return template.getForObject(HTTP_SERVICE_URL + ":" + PORT + API + VALUE, String.class);
+	}
 }
